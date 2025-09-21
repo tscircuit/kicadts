7db743c05f0e40d2c57c5fac4344b186e0863bd4
seveibar <seveibar@gmail.com>
2025-09-21 13:15:58 -0700

    kicad sexpr details in agents

---
+++ AGENTS.md
@@ -0,0 +1,11 @@
+This is the `kicadts` typescript library. It's still in early developement and
+we're trying to make sure we parse the entire KiCad S-expression specification.
+
+You can find the difference specifications in the `references` directory, the
+most extensive one is `references/SEXPR_MAIN.adoc`.
+
+- `references/SCHEMATIC_SEXPR.adoc`
+- `references/PCB_SEXPR.adoc`
+- `references/FOOTPRINT_SEXPR.adoc`
+- `references/SCH_SYM_SEXPR.adoc`
+- `references/SEXPR_MAIN.adoc`
---
+++ scripts/download-references.ts
@@ -0,0 +1,8 @@
+// This script should download the following files and place them into the
+// references directory with appropriate names
+
+// https://gitlab.com/kicad/services/kicad-dev-docs/-/raw/master/content/file-formats/sexpr-schematic/_index.en.adoc
+// https://gitlab.com/kicad/services/kicad-dev-docs/-/raw/master/content/file-formats/sexpr-intro/_index.en.adoc
+// https://gitlab.com/kicad/services/kicad-dev-docs/-/blob/master/content/file-formats/sexpr-footprint/_index.en.adoc
+// https://gitlab.com/kicad/services/kicad-dev-docs/-/raw/master/content/file-formats/sexpr-pcb/_index.en.adoc
+// https://gitlab.com/kicad/services/kicad-dev-docs/-/raw/master/content/file-formats/sexpr-symbol-lib/_index.en.adoc
